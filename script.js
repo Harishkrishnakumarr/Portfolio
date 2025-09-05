c49c7fe5a0ffb9ac51f6ef30e9bf5e17a9c19366
Harishkrishnakumarr <harishkrishnakumarr@gmail.com>
2025-09-05 13:55:49 +0530

    little changes in the sidebar

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const navMenu = document.getElementById('nav-menu');
     const overlay = document.querySelector('.overlay');
 
-    // Toggle the menu and overlay when the hamburger is clicked
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('show');
         overlay.classList.toggle('show');
     });
 
-    // Close the menu and overlay when a link is clicked
     const navLinks = navMenu.querySelectorAll('a');
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
@@ -18,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    // Close the menu and overlay when the overlay is clicked
     overlay.addEventListener('click', () => {
         navMenu.classList.remove('show');
         overlay.classList.remove('show');
